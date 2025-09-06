d0b3e499494ed21105a1f72c833411e6eac7c428
Edward Nafornita <contact@edwardnafornita.com>
2025-09-06 18:35:20 -0400

    fix: betterfox integration with firefox

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }
+{ pkgs, ... }:
 
 let
   betterfox =  pkgs.fetchFromGitHub {
     owner = "yokoffing";
     repo = "Betterfox";
-    rev = 7b3cee24cc7438eef040270b0464cde932e98ffb
-    sha256 = "sha256-n4bqwdb55l8skw5dbb08rv5yizz9iq10=";
+    rev = "7b3cee24cc7438eef040270b0464cde932e98ffb";
+    sha256 = "sha256-aTNEsopnGVueM7MZaYMPUMSJDJeADpvM33UcgO8nz88=";
   };
 
   core = builtins.readFile (betterfox + "/user.js");
@@ -24,16 +24,12 @@ in
 	user_pref("privacy.resistFingerprinting.letterboxing", true);
       '';
 
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-        ublock-origin bitwarden darkreader
-      ];
-
-      policies = {
-        DisableTelemetry = true;
-	DisableFirefoxStudies = true;
-	DisablePocket = true;
-	PasswordManagerEnabled = false;
-      };
+    };
+    
+    policies = {
+      DisableTelemetry = true;
+      DisablePocket = true;
+      PasswordManagerEnabled = false;
     };
   };
 }
