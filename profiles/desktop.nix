10dce367ec9b7c2d0d66ba4d083403d6762d15b6
Edward Nafornita <contact@edwardnafornita.com>
2025-09-06 17:24:44 -0400

    fix: keep HM modules under users/edward/home.nix; system-only in profiles

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -5,10 +5,6 @@
     ../modules/system/base.nix
     ../modules/system/networking.nix
     ../modules/system/desktop-hyprland.nix
-
-    ../modules/home/base.nix
-    ../modules/home/hyprland.nix
-    ../modules/home/waybar.nix
   ];
 }
 
