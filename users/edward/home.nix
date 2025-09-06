f61412ecc047bd55b4183f235c4a5af772caa63a
Edward Nafornita <contact@edwardnafornita.com>
2025-09-06 17:34:33 -0400

    fix: home-manager now appends .bak to conflicting files

--- users/edward/home.nix
+++ users/edward/home.nix
@@ -3,15 +3,16 @@
 {
   home.username = "edward";
   home.homeDirectory = "/home/edward";
+  home.stateVersion = "25.11";
 
   programs.home-manager.enable = true;
+
+  home-manager.backupFileExtension = "bak";
 
   imports = [
     ../../modules/home/base.nix
     ../../modules/home/hyprland.nix
     ../../modules/home/waybar.nix
   ];
-
-  home.stateVersion = "25.11";
 }
 
